b8c925f159c30d20df4784572c38be4c07d8b50c
Watinguyaz <watimakhanu@gmail.com>
2019-01-28 04:33:22 +0300

    doctrrings added

--- user_test.py
+++ user_test.py
@@ -1,3 +1,11 @@
 import unittest
 from user from User
 class TestUser(unittest.TestCase):
+
+    """
+    Test class that defines test cases for the user class behaviours,
+    the arguments help in creating test cases.
+
+    """
+
+    
