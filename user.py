0ac9281a1c656ed32c8c2e38e008cc263a1aae57
Watinguyaz <watimakhanu@gmail.com>
2019-01-28 04:04:49 +0300

    user class created

---
+++ user.py
@@ -0,0 +1,20 @@
+class User:
+    """
+
+    Class that will generate new instances of users
+
+    """
+
+    def __init__(self,login_username):
+
+        """
+        the __init__method helps us define properties for our objectsself.
+
+        """
+
+        self.login_username = login_username
+
+        """
+        arguments for our __init__method will include the following.
+
+        """
